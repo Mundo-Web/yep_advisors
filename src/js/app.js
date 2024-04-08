bae18743acc821dcb39523f5e04a56ff99d6944d
Jordan Daniel 27 <jordanmoreano.ppy@gmail.com>
2024-04-08 09:21:34 -0500

    Archivos cambiados a la ruta app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,84 +1,30 @@
-const wealth = document.getElementById("wealth");
-const finanzas = document.getElementById("finanzas");
-const estructuracion = document.getElementById("estructuracion");
-const services = document.querySelectorAll(".services");
-const imagesServicios = document.querySelectorAll(".images-servicios");
+document.addEventListener("DOMContentLoaded", function () {
+  const enlaces = document.querySelectorAll(".enlace");
+  const services = document.querySelectorAll(".services");
 
-// Clases a agregar y quitar
-const addClasses = [
-  "text-colorSubtitleLittle",
-  "font-semibold",
-  "text-littleTitle",
-];
-const removeClasses = [
-  "font-regular",
-  "text-regularSize",
-  "text-colorSubtitleLittle",
-  "font-semibold",
-  "text-littleTitle",
-];
+  // Función para manejar el clic en un enlace
+  function handleClick(e) {
+    const targetId = e.target.id;
+    services.forEach((serv) => {
+      const hasClass = serv.classList.contains(targetId);
+      serv.classList.toggle("hidden", !hasClass);
+    });
 
-loadEventListeners();
+    enlaces.forEach((enlace) => {
+      const isSelected = enlace.id === targetId;
+      enlace.classList.toggle("text-colorSubtitleLittle", isSelected);
+      enlace.classList.toggle("text-littleTitle", isSelected);
+      enlace.classList.toggle("font-semibold", isSelected);
+      enlace.classList.toggle("text-regularSize", !isSelected);
+      enlace.classList.toggle("font-regular", !isSelected);
+    });
+  }
 
-function loadEventListeners() {
-  wealth.addEventListener("click", showWealthManagement);
-  finanzas.addEventListener("click", showFinanzas);
-  estructuracion.addEventListener("click", showEstructuras);
-}
-
-function showFinanzas() {
-  services[0].classList.add("hidden");
-  services[1].classList.remove("hidden");
-  services[2].classList.add("hidden");
-
-  imagesServicios[0].classList.add("hidden");
-  imagesServicios[1].classList.remove("hidden");
-  imagesServicios[2].classList.add("hidden");
-
-  updateElementClasses(finanzas, addClasses, removeClasses);
-  deleteElementClasses(wealth, estructuracion);
-}
-
-function showWealthManagement() {
-  console.log("wealth");
-
-  services[0].classList.remove("hidden");
-  services[1].classList.add("hidden");
-  services[2].classList.add("hidden");
-
-  imagesServicios[0].classList.remove("hidden");
-  imagesServicios[1].classList.add("hidden");
-  imagesServicios[2].classList.add("hidden");
-
-  updateElementClasses(wealth, addClasses, removeClasses);
-  deleteElementClasses(finanzas, estructuracion);
-}
-
-function showEstructuras() {
-  console.log("estructuras");
-
-  services[0].classList.add("hidden");
-  services[1].classList.add("hidden");
-  services[2].classList.remove("hidden");
-
-  imagesServicios[0].classList.add("hidden");
-  imagesServicios[1].classList.add("hidden");
-  imagesServicios[2].classList.remove("hidden");
-
-  // Llamar a la función para actualizar las clases
-  updateElementClasses(estructuracion, addClasses, removeClasses);
-  deleteElementClasses(finanzas, wealth);
-}
-
-function updateElementClasses(element, addClasses, removeClasses) {
-  element.classList.remove(...removeClasses);
-  element.classList.add(...addClasses);
-}
-
-function deleteElementClasses(elementFinanza, elementWealth) {
-  elementFinanza.classList.remove(...addClasses);
-  elementWealth.classList.remove(...addClasses);
-}
+  // Agregar el event listener a cada enlace
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", handleClick);
+  });
+});
 
 function show() {
   document.querySelector(".hamburger").classList.toggle("open");
